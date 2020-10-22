bb188dba04a45b24222503c3006400c2deef58db
Clara01m <clara.mahlgard.sthlm.ntig@gmail.com>
2020-10-22 16:30:42 +0200

    Last changes i made

--- Prov/Book.cs
+++ Prov/Book.cs
@@ -14,21 +14,27 @@ namespace Prov
 
         public Book()
         {
-
-
+            actualValue =generator.Next(10,101);
+            rarity = generator.Next(11);
         }
         public void PrintInfo()
         {
-
+            Console.WriteLine(Book.name);
+            Console.WriteLine(Book.rarity);
+            Console.WriteLine(Book.category);
+            Console.WriteLine(price);
+            
         }
         //Fixing the following 4 once the other class is created and working
-        public int Evaluate()
+        public int Evaluate(float truePrice)
         {
- 
+            price= actualValue*rarity;
+            truePrice = (float) (generator.NextDouble() * 1.5);  
         }
         public string GetCategory()
         {
-
+            string[] categoryArray{"Old book", "Treasure map", "Scroll"};
+            category= categoryArray[generator.Next(4)]; 
         }
         public string GetName()
         {
